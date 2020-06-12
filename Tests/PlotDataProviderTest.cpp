5e36432884ae56ef8ef82a5cd9e0f4e5ed59b5d5
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-06-12 15:55:38 +0200

    Added test PlotDataProviderTest::testRecomputeGroupingDataWrongColumnFormat().

--- Tests/PlotDataProviderTest.cpp
+++ Tests/PlotDataProviderTest.cpp
@@ -20,3 +20,29 @@ void PlotDataProviderTest::testRecomputeGroupingDataNotSetColumn()
                                    ColumnType::STRING);
     QCOMPARE(spy.count(), NO_SIGNAL);
 }
+
+void PlotDataProviderTest::testRecomputeGroupingDataWrongColumnFormat()
+{
+    checkRecomputeGroupingDataForColumnType(ColumnType::DATE);
+    checkRecomputeGroupingDataForColumnType(ColumnType::NUMBER);
+    checkRecomputeGroupingDataForColumnType(ColumnType::UNKNOWN);
+}
+
+void PlotDataProviderTest::checkRecomputeGroupingDataForColumnType(
+    ColumnType columnType)
+{
+    PlotDataProvider provider;
+    QSignalSpy spy(&provider, &PlotDataProvider::groupingPlotDataChanged);
+    provider.recomputeGroupingData(calcData_, 1, columnType);
+    QCOMPARE(spy.count(), SIGNAL);
+    checkSignalParamsForRecomputeGroupingAreEmpty(spy.first());
+}
+
+void PlotDataProviderTest::checkSignalParamsForRecomputeGroupingAreEmpty(
+    const QList<QVariant>& signalParameters)
+{
+    QCOMPARE(signalParameters.size(), 3);
+    QCOMPARE(signalParameters[0].value<QVector<int>>(), {});
+    QCOMPARE(signalParameters[1].value<QVector<int>>(), {});
+    QCOMPARE(signalParameters[2].value<Quantiles>().count_, Quantiles().count_);
+}
--- Tests/PlotDataProviderTest.h
+++ Tests/PlotDataProviderTest.h
@@ -1,8 +1,11 @@
 #ifndef PLOTDATAPROVIDERTEST_H
 #define PLOTDATAPROVIDERTEST_H
 
+#include <ColumnType.h>
 #include <QObject>
 #include <QSignalSpy>
+
+#include "TransactionData.h"
 
 class PlotDataProviderTest : public QObject
 {
@@ -13,9 +16,20 @@ public:
 private Q_SLOTS:
     void testNotSetGroupingColumn();
     void testRecomputeGroupingDataNotSetColumn();
+    void testRecomputeGroupingDataWrongColumnFormat();
 
 private:
+    void checkRecomputeGroupingDataForColumnType(ColumnType columnType);
+    void checkSignalParamsForRecomputeGroupingAreEmpty(
+        const QList<QVariant>& signalParameters);
+
     static constexpr int NO_SIGNAL{0};
+    static constexpr int SIGNAL{1};
+
+    const QVector<TransactionData> calcData_{
+        {QDate(1, 3, 2010), QVariant("column1"), 10},
+        {QDate(4, 3, 2010), QVariant("column1"), 15},
+        {QDate(6, 3, 2010), QVariant("column1"), 12}};
 };
 
 #endif  // PLOTDATAPROVIDERTEST_H
