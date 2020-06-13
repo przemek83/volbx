c6d28ef20cc2748b059c2d1c964f52ac3e068b81
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-06-13 13:11:42 +0200

    Added PlotDataProviderTest::testRecompute(). Small refactor in PlotDataProviderTest class.

--- Tests/PlotDataProviderTest.cpp
+++ Tests/PlotDataProviderTest.cpp
@@ -9,15 +9,15 @@ PlotDataProviderTest::PlotDataProviderTest(QObject* parent) : QObject(parent) {}
 void PlotDataProviderTest::initTestCase()
 {
     mainQuantiles_.max_ = 15.;
-    mainQuantiles_.min_ = 10.;
-    mainQuantiles_.q10_ = 10.40;
-    mainQuantiles_.q25_ = 11.;
-    mainQuantiles_.q50_ = 12.;
-    mainQuantiles_.q75_ = 13.50;
-    mainQuantiles_.q90_ = 14.40;
-    mainQuantiles_.mean_ = 12.33;
-    mainQuantiles_.count_ = 3;
-    mainQuantiles_.stdDev_ = 2.05;
+    mainQuantiles_.min_ = 1.;
+    mainQuantiles_.q10_ = 1.50;
+    mainQuantiles_.q25_ = 2.75;
+    mainQuantiles_.q50_ = 7.50;
+    mainQuantiles_.q75_ = 11.50;
+    mainQuantiles_.q90_ = 13.50;
+    mainQuantiles_.mean_ = 7.50;
+    mainQuantiles_.count_ = 6;
+    mainQuantiles_.stdDev_ = 5.19;
 
     firstQuantiles_.max_ = 15.;
     firstQuantiles_.min_ = 10.;
@@ -60,13 +60,7 @@ void PlotDataProviderTest::testRecomputeGroupingData()
     QSignalSpy spy(&provider, &PlotDataProvider::groupingPlotDataChanged);
     provider.recomputeGroupingData(calcData_, ColumnType::STRING);
     QCOMPARE(spy.count(), SIGNAL);
-    QList<QVariant>& signalParameters{spy.first()};
-    QCOMPARE(signalParameters.size(), 3);
-    QCOMPARE(signalParameters[0].value<QVector<QString>>(),
-             QVector({QString("column1"), QString("column2")}));
-    QCOMPARE(signalParameters[1].value<QVector<Quantiles>>(),
-             QVector({firstQuantiles_, secondQuantiles_}));
-    QCOMPARE(signalParameters[2].value<Quantiles>(), Quantiles());
+    checkSignalParametersForRecomputeGrouping(spy.first(), Quantiles());
 }
 
 void PlotDataProviderTest::testRecomputeGroupingDataEmptyCalcData()
@@ -111,6 +105,35 @@ void PlotDataProviderTest::testRecomputeEmptyCalcData()
     QCOMPARE(signalParameters[1].value<Quantiles>(), Quantiles());
 }
 
+void PlotDataProviderTest::testRecompute()
+{
+    PlotDataProvider provider;
+    QSignalSpy groupingPlotDataChangedSpy(
+        &provider, &PlotDataProvider::groupingPlotDataChanged);
+    QSignalSpy basicPlotDataChangedSpy(&provider,
+                                       &PlotDataProvider::basicPlotDataChanged);
+    QSignalSpy fundamentalDataChangedSpy(
+        &provider, &PlotDataProvider::fundamentalDataChanged);
+    provider.recompute(calcData_, ColumnType::STRING);
+
+    QCOMPARE(groupingPlotDataChangedSpy.count(), SIGNAL);
+    checkSignalParametersForRecomputeGrouping(
+        groupingPlotDataChangedSpy.first(), mainQuantiles_);
+
+    QCOMPARE(basicPlotDataChangedSpy.count(), SIGNAL);
+    QList<QVariant>& signalParameters{basicPlotDataChangedSpy.first()};
+    QCOMPARE(signalParameters.size(), 3);
+    QCOMPARE(signalParameters[0].value<QVector<QPointF>>(), points_);
+    QCOMPARE(signalParameters[1].value<Quantiles>(), mainQuantiles_);
+    QCOMPARE(signalParameters[2].value<QVector<QPointF>>(), regression_);
+
+    QCOMPARE(fundamentalDataChangedSpy.count(), SIGNAL);
+    signalParameters = fundamentalDataChangedSpy.first();
+    QCOMPARE(signalParameters.size(), 2);
+    QCOMPARE(signalParameters[0].value<QVector<double>>(), yAxisValues_);
+    QCOMPARE(signalParameters[1].value<Quantiles>(), mainQuantiles_);
+}
+
 void PlotDataProviderTest::checkRecomputeGroupingDataForColumnType(
     ColumnType columnType)
 {
@@ -128,4 +151,15 @@ void PlotDataProviderTest::checkSignalParamsForRecomputeGroupingAreEmpty(
     QCOMPARE(signalParameters[0].value<QVector<QString>>(), {});
     QCOMPARE(signalParameters[1].value<QVector<Quantiles>>(), {});
     QCOMPARE(signalParameters[2].value<Quantiles>(), Quantiles());
+}
+
+void PlotDataProviderTest::checkSignalParametersForRecomputeGrouping(
+    const QList<QVariant>& signalParameters, Quantiles quantiles)
+{
+    QCOMPARE(signalParameters.size(), 3);
+    QCOMPARE(signalParameters[0].value<QVector<QString>>(),
+             QVector({QString("column1"), QString("column2")}));
+    QCOMPARE(signalParameters[1].value<QVector<Quantiles>>(),
+             QVector({firstQuantiles_, secondQuantiles_}));
+    QCOMPARE(signalParameters[2].value<Quantiles>(), quantiles);
 }
--- Tests/PlotDataProviderTest.h
+++ Tests/PlotDataProviderTest.h
@@ -2,6 +2,7 @@
 #define PLOTDATAPROVIDERTEST_H
 
 #include <QObject>
+#include <QPointF>
 #include <QSignalSpy>
 
 #include <ColumnType.h>
@@ -23,26 +24,39 @@ private Q_SLOTS:
     void testRecomputeGroupingDataEmptyCalcData();
 
     void testRecomputeEmptyCalcData();
+    void testRecompute();
 
 private:
     void checkRecomputeGroupingDataForColumnType(ColumnType columnType);
     void checkSignalParamsForRecomputeGroupingAreEmpty(
         const QList<QVariant>& signalParameters);
 
+    void checkSignalParametersForRecomputeGrouping(
+        const QList<QVariant>& signalParameters, Quantiles quantiles);
+
     static constexpr int NO_SIGNAL{0};
     static constexpr int SIGNAL{1};
 
     const QVector<TransactionData> calcData_{
-        {QDate(1, 3, 2010), QVariant("column1"), 10},
-        {QDate(4, 3, 2010), QVariant("column1"), 15},
-        {QDate(6, 3, 2010), QVariant("column1"), 12},
-        {QDate(1, 3, 2010), QVariant("column2"), 1},
-        {QDate(4, 3, 2010), QVariant("column2"), 5},
-        {QDate(6, 3, 2010), QVariant("column2"), 2}};
+        {QDate(2010, 3, 1), QVariant("column1"), 10},
+        {QDate(2010, 3, 4), QVariant("column1"), 15},
+        {QDate(2010, 3, 6), QVariant("column1"), 12},
+        {QDate(2010, 3, 1), QVariant("column2"), 1},
+        {QDate(2010, 3, 4), QVariant("column2"), 5},
+        {QDate(2010, 3, 6), QVariant("column2"), 2}};
 
     Quantiles mainQuantiles_;
     Quantiles firstQuantiles_;
     Quantiles secondQuantiles_;
+
+    QVector<QPointF> points_{QPointF(14669, 10), QPointF(14672, 15),
+                             QPointF(14674, 12), QPointF(14669, 1),
+                             QPointF(14672, 5),  QPointF(14674, 2)};
+
+    QVector<QPointF> regression_{QPointF(14669, 6.44736842105),
+                                 QPointF(14674, 8.42105263158)};
+
+    QVector<double> yAxisValues_{10., 15., 12., 1., 5., 2.};
 };
 
 #endif  // PLOTDATAPROVIDERTEST_H
