f74e360a21bf3eeea21ef8d51319ff2394c7244c
Przemek <4788832+przemek83@users.noreply.github.com>
2022-12-21 12:42:32 +0100

    Update Constants.cpp
    Removed static memory usage for progress bar names.

--- Common/Constants.cpp
+++ Common/Constants.cpp
@@ -17,20 +17,11 @@ QString getTemporaryFileSuffix() { return QStringLiteral(".tmp"); }
 
 QString getConfigurationFileName() { return QStringLiteral("config"); }
 
-static QVector<QString> initProgressBarNames()
-{
-    QVector<QString> progressTitles(static_cast<int>(BarTitle::END));
-    progressTitles[static_cast<int>(BarTitle::LOADING)] =
-        QObject::tr("Loading");
-    progressTitles[static_cast<int>(BarTitle::SAVING)] = QObject::tr("Saving");
-    progressTitles[static_cast<int>(BarTitle::ANALYSING)] =
-        QObject::tr("Analysing");
-    return progressTitles;
-}
-
 QString getProgressBarTitle(BarTitle barTitle)
 {
-    static const QVector<QString> progressTitles{initProgressBarNames()};
+    const QVector<QString> progressTitles{QObject::tr("Loading"),
+                                          QObject::tr("Saving"),
+                                          QObject::tr("Analysing")};
     return progressTitles[static_cast<int>(barTitle)];
 }
 
