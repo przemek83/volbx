d45c7630af20b072e5e8469be5608f982d6328f5
przemek83 <4788832+przemek83@users.noreply.github.com>
2025-01-15 08:35:45 +0100

    Remove declarations of copy/move ctor, assignment operators and dtor.

--- src/Shared/Logger.h
+++ src/Shared/Logger.h
@@ -19,12 +19,6 @@ class Logger : public QObject
 {
     Q_OBJECT
 public:
-    Logger& operator=(const Logger& other) = delete;
-    Logger(const Logger& other) = delete;
-
-    Logger& operator=(Logger&& other) = delete;
-    Logger(Logger&& other) = delete;
-
     static Logger& getInstance();
 
     void log(LogTypes type, const char* file, const char* function, int line,
@@ -34,7 +28,6 @@ public:
 
 private:
     Logger();
-    ~Logger() override = default;
 
     QTextEdit* createLogsTextEdit();
 
